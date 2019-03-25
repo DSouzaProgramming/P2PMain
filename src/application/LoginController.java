a3ab7ade6ac042f2b4ab824afca02b0cefb05d74
Dylan Souza <dylan_anthony@live.com>
2019-03-25 09:05:26 -0400

    first push

---
+++ src/application/LoginController.java
@@ -0,0 +1,43 @@
+package application;
+import javafx.fxml.*;
+import javafx.scene.control.*;
+import javafx.util.*;
+import javafx.application.*;
+
+public class LoginController {
+	
+	@FXML
+	private Button login;
+	
+	@FXML
+	private Button register;
+	
+	@FXML
+	private TextField username;
+	
+	@FXML
+	private TextField password;
+
+	public LoginController()
+	{
+		
+	}
+	
+	@FXML
+	private void initialize()
+	{
+		
+	}
+	
+	@FXML
+	private void verifyLogin()
+	{
+		
+	}
+	
+	@FXML
+	private void addUser()
+	{
+		
+	}
+}
